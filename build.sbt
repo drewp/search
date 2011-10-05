79476fbb638669b179a8042ef7c1dd03e15d601b
drewp <drewp@bigasterisk.com>
2011-10-04 22:04:15 -0700

    upgrade scala and sbt. new title field support. better row template
    Ignore-this: a43740aa7c47e1113d08360129818c11
    
    darcs-hash:20111005050415-312f9-f8b09e3036626caadfd3c5fd5f2936b39e104f5d.gz

---
+++ build.sbt
@@ -0,0 +1,32 @@
+name := "search"
+
+version := "1.0"
+
+scalaVersion := "2.9.1"
+
+mainClass := Some("JettyLauncher")
+
+seq(webSettings :_*)
+
+seq(assemblySettings: _*)
+
+libraryDependencies ++= Seq(
+  "org.scalatra" %% "scalatra" % "2.0.1",
+  "org.scalatra" %% "scalatra-specs" % "2.0.1" % "test",
+  "org.mortbay.jetty" % "jetty" % "6.1.22" % "compile, jetty",
+  "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "provided-> default",
+  "javax.servlet" % "servlet-api" % "2.5" % "provided->default",
+  "net.databinder" %% "dispatch-http" % "0.7.8",
+  "org.slf4j" % "slf4j-api" % "1.6.1",
+  "org.slf4j" % "slf4j-simple" % "1.6.1",
+  "net.liftweb" %% "lift-json" % "2.4-M4",
+  "com.mongodb.casbah" % "casbah_2.9.0-1" % "2.1.5.0"
+)
+
+resolvers ++= Seq(
+  "Sonatype OSS" at "http://oss.sonatype.org/content/repositories/releases/",
+  "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
+  "Web plugin repo" at "http://siasia.github.com/maven2"
+)
+
+jettyPort := 9999
--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -1,15 +1,13 @@
 import org.scalatra._
 import dispatch._
 import Http._
-import net.liftweb.json.JsonAST._
+import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
-import net.liftweb.json.JsonParser._
-import net.liftweb.json.Merge
 import java.net.URLEncoder
 
 
 case class Highlight(text: List[String])
-case class Source(source: String, view: Option[String])
+case class Source(source: String, view: Option[String], title: Option[String])
 case class Hit(_score: Double, _index: String, _id: String, _type: String, 
 	       _source: Source, highlight: Highlight)
 case class Hits(hits: List[Hit], total: Int)
@@ -26,24 +24,27 @@ class SearchApp extends ScalatraServlet {
   // separate the data sources
   val elasticTypeName = "doc"; 
 
-  before {
-    contentType = "application/json"
-  }
+// need to make the index like this:
+// curl -XPUT http://bang:9200/main/ -d '{"settings":{"index":{"number_of_shards":1}}}'
 
   post("/index") {
     /*
      param source=<data source name>
-     body is a json doc with uri/text and optional title and modified
+     body is a json doc with uri/text and optional title and modified.
+     The title will not be searched, so you have to repeat that text
+     inside. I'm not sure how I'll clean that repetition up for
+     results display.
 
      Existing doc with the same uri will be overwritten. source is a
      url param, not a doc attribute, in case someday I want to use it
      to pick elastic type or index instead
      */
-    val doc = parse(request.body) merge ("source" -> params("source"))
-    val uri = (doc \ "uri").children(0).asInstanceOf[JString].s
+    val s: JValue = ("source" -> params("source"))
+    val doc = parse(request.body) merge s
+    val uri = (doc \ "uri").asInstanceOf[JString].s
     
-    val http = new Http
-    http.x(elastic / elasticTypeName / URLEncoder.encode(uri, "UTF-8") <<< 
+    contentType = "application/json"
+    Http.x(elastic / elasticTypeName / URLEncoder.encode(uri, "UTF-8") <<< 
 	   compact(render(doc)) as_str)
   }
 
@@ -53,8 +54,8 @@ class SearchApp extends ScalatraServlet {
     // todo: this is supposed to be a GET, but I think elastic is ok
     // with me sending the wrong method. I couldn't find how to force
     // the method name in dispatch.
-    val http = new Http
-    http.x(elastic / "_search" <<? Map("_pretty" -> "true") << compact(render(
+    contentType = "application/json"
+    Http.x(elastic / "_search" <<? Map("_pretty" -> "true") << compact(render(
       ("query" -> ("term" -> ( "source" -> params("source") )))
     )) as_str)
   }
@@ -64,8 +65,6 @@ class SearchApp extends ScalatraServlet {
      param source=s1,s2,s3, or omit to search everything
      param q=text, where text is to be found in the 'text' attr of the docs
     */
-    val http = new Http
-
     val jsonQuery = compact(render(
       ("query" -> ("query_string" -> (
 	// this analyzer, whatever it is, allows the user to search on
@@ -88,24 +87,26 @@ class SearchApp extends ScalatraServlet {
     ))
     println(jsonQuery)
     val elasticResponse = parse(
-      http.x(elastic / "_search" <<? Map("source" -> jsonQuery) as_str)
+      Http.x(elastic / "_search" <<? Map("source" -> jsonQuery) as_str)
     )
     // errors from elastic are just falling right through, currently
     val extracted = elasticResponse.extract[HitsResponse]
   
-    compact(render(
-      ("hits" -> 
-       extracted.hits.hits.map(h => Merge.merge(
+    val hits = extracted.hits.hits.map(h => Merge.merge(
 	 ("uri" -> h._id) ~
+         ("title" -> h._source.title.getOrElse(h._id)) ~
 	 ("text" -> h.highlight.text.mkString(" ")) ~
 	 ("source" -> h._source.source),
 	 h._source.view match {
 	   case None => JNothing
-	   case Some(view) => ("view" -> view)
-	 }))) ~
-      ("sourceTotals" -> 
-       JObject(extracted.facets.source.terms.map(
-	 t => JField(t.term, t.count)))) ~
+	   case Some(view) => ("view" -> view): JValue
+	 }))
+
+    contentType = "application/json"
+    compact(render(
+      ("hits" -> hits) ~
+      ("sourceTotals" -> JObject(extracted.facets.source.terms.map(
+	t => JField(t.term, t.count)))) ~
       ("totalHits" -> extracted.hits.total) ~
       ("took" -> extracted.took)
     ))
