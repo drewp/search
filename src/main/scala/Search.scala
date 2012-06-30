aae4be2c58f5b04a5ee1d8b2db017514622e1f75
drewp <drewp@bigasterisk.com>
2012-06-30 13:00:47 -0700

    take out the exit-on-index
    Ignore-this: f91cbd2aec9331086e3f726b646733b9
    
    darcs-hash:20120630200047-312f9-c775c08a5362a3b1605d72c2d864821acac19d4a.gz

--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -43,7 +43,7 @@ class SearchApp extends ScalatraServlet {
     val doc = parse(request.body) merge s
     val uri = (doc \ "uri").asInstanceOf[JString].s
     
-;{  ;exit()};    contentType = "application/json"
+    contentType = "application/json"
     Http.x(elastic / elasticTypeName / URLEncoder.encode(uri, "UTF-8") <<< 
 	   compact(render(doc)) as_str)
   }
